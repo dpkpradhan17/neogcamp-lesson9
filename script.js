547ef7c66136cfdc91fcebb58990d90ebf19fe69
dpkpradhan17 <dpkpradhan17@gmail.com>
2022-10-01 12:28:19 +0530

    ex-04 done

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ function convertDateToStr(date){
     dateStr.year=date.year.toString();
     return dateStr;
 }
+
+function getAllDateFormats(date){
+    var dateStr = convertDateToStr(date);
+
+    var ddmmyyyy = dateStr.day + dateStr.month +dateStr.year;
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2);
+    var mmddyy= dateStr.month + dateStr.day + dateStr.year.slice(-2);
+    var yymmdd=dateStr.year.slice(-2) + dateStr.month + dateStr.day;
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy,mmddyy,yymmdd,]
+}
+
+var date={
+    day: 5,month: 6, year: 1996,
+}
+console.log(getAllDateFormats(date));
